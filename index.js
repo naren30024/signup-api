c7ec3a005df08cc71b6d6f556fe7e4413b6ded75
naren30024 <naren30024@gmail.com>
2025-04-11 13:43:05 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require("express")
+var bcrypt = require("bcrypt")
+var mysql = require("mysql")
+var app = express()
+
+app.use(express.json())
+
+var connection = mysql.createConnection({
+    username:"",
+    password:"",
+    database:"",
+    port:"",
+})
+
+
+app.post("/register",(req,res) =>{
+    const {userName, email, password} = req.body;
+    if(length(password) < 8){
+        res.send({"data":"password too short"})
+    }
+    const checkUser = `select * from users where email=${email}`
+    if(checkUser === undefined){
+        res.send({"data":"user already exist"})
+    }
+    const hashedPassword = bcrypt.hash(password,10);
+    const queryRegister = `inser into users('username','email','password') values (${userName}, ${email}, ${password})`
+    
+    connection.query(queryRegister,(Error,result)=>{
+        if(Error){
+            throw Error;
+        } 
+
+        res.status(200);
+        res.send(result.json())
+    });
+    
+   
+})
